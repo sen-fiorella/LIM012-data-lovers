04f402a0bc1d7f4ca8ccc1d4d44a891f12d8edc9
MarilynAguilar <marilyncarolayaguilar@gmail.com>
2020-03-17 17:39:06 -0500

    Estructura html, css, js de historia 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-
-console.log(example, data);
+// import { allChampions } from './data.js';
+// import copyLol from './data/lol/lol.js';
+// const dataLOL = allChampions(copyLol.data);
+ 
+// console.log(dataLOL);
+ 
+ 
+//vista1=Página principal (manipulación dinámica del DOM)
+const viewLogin = document.getElementById("viewLogin");
+//vista2
+const viewChampions = document.getElementById("viewChampions");
+//Se ingresa nombre en input(User)
+const inputName = document.getElementById("inputName");
+const buttonEnter = document.getElementById("buttonEnter");
+let nameInvocador = document.getElementById("nameInvocador");
+// const contaner = document.getElementById("contaner");
+ 
+ 
+//Evento que al hacer click en el botón recoge el nombre (userName) y lo inserta en la siguiente vista
+buttonEnter.addEventListener("click", () => {
+    let userName = inputName.value;
+    nameInvocador.innerHTML += userName;
+    viewChampions.classList.remove("hide");
+    viewLogin.classList.add("hide");   
+});
+ 
+// export const allChampions = (arrayLOL) => {
+//     contaner.innerHTML = "";
+//     for (let i = 0; i < arrayLOL.length; i++) {
+//     const nombre = arrayLOL[i].name;
+//     contaner.innerHTML += nombre;
+//     }
+ 
+// };
