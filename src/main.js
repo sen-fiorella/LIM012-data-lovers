ac839fcf8981018fe9e4c8f38666b09166102a88
MarilynAguilar <marilyncarolayaguilar@gmail.com>
2020-03-31 16:24:09 -0500

    Modificando html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // import { allChampions } from './data.js';
 import copyLol from './data/lol/lol.js';
-import { alphabetOrder } from './data.js';
-
+import {
+  alphabetOrder,
+  roleFilter,
+} from './data.js';
 
 const dataLol = (copyLol.data);
 // Declaramos las variables - Manipulación dinámica del DOM
@@ -9,38 +11,94 @@ const viewLogin = document.getElementById('viewLogin');
 const viewChampions = document.getElementById('viewChampions');
 const inputName = document.getElementById('inputName');
 const buttonEnter = document.getElementById('buttonEnter');
-const nameInvocador = document.getElementById('nameInvocador');
+const invocadorName = document.getElementById('invocadorName');
 const container = document.getElementById('container');
+const myModal = document.getElementById('myModal');
+const modalInfo = document.getElementById('modaInfo');
 
+
+// Historia 1
 // Evento que recoge el nombre del usuario al hacer click y se inserta en la siguiente vista:
 buttonEnter.addEventListener('click', () => {
   const userName = inputName.value;
-  nameInvocador.innerHTML += userName;
+  invocadorName.innerHTML += userName;
   viewChampions.classList.remove('hide');
   viewLogin.classList.add('hide');
 });
+
 // Historia 2 - Muestra a los campeones (nombre e imagen)
 const data = Object.values(dataLol);
 const showData = (parametro) => {
   let show = '';
   parametro.forEach((element) => {
     const campeones = `            
-            <div class="champ">
-            <img src=${element.splash} class="splash"/> 
-            <h1 class="nameChampions"> ${element.name}<h1>
-            </div>`;
+      <div class="champ" name="champion">
+      <img src=${element.splash} class="splash"/> 
+      <h1 class="championsName">${element.name} </h1>
+      </div>`;
     show += campeones;
   });
   container.innerHTML = show;
 };
 showData(data);
 
-
+// Historia 3 - Ordenar (A-Z / Z-A)
 const buttonOrder = document.querySelector('#buttonOrder');
-// utilizando querySelector y getelementebyid funcions en boton
-// Historia 3 - dando vida al boton de ordenar
 buttonOrder.addEventListener('change', () => {
   const valueOrder = buttonOrder.value;
   container.innerHTML = '';
   showData(alphabetOrder(data, valueOrder));
+});
+
+// Historia 4 - Filtrar por Roles
+const assassin = document.getElementById('assassin');
+assassin.addEventListener('click', () => {
+  container.innerHTML = '';
+  showData(roleFilter(data, 'Assassin'));
+});
+
+const fighter = document.getElementById('fighter');
+fighter.addEventListener('click', () => {
+  container.innerHTML = '';
+  showData(roleFilter(data, 'Fighter'));
+});
+
+const mage = document.getElementById('mage');
+mage.addEventListener('click', () => {
+  container.innerHTML = '';
+  showData(roleFilter(data, 'Mage'));
+});
+
+const marksman = document.getElementById('marksman');
+marksman.addEventListener('click', () => {
+  container.innerHTML = '';
+  showData(roleFilter(data, 'Marksman'));
+});
+
+const support = document.getElementById('support');
+support.addEventListener('click', () => {
+  container.innerHTML = '';
+  showData(roleFilter(data, 'Support'));
+});
+
+const tank = document.getElementById('tank');
+tank.addEventListener('click', () => {
+  container.innerHTML = '';
+  showData(roleFilter(data, 'Tank'));
+});
+
+// Modal
+container.addEventListener('click', () => {
+  const position = parseInt(event.target.parentElement.id - 1);
+  // se usa para la delegación de un evento, esta es la condición
+  if (event.target.parentElement.getAttribute('name') === 'champion') {
+    myModal.classList.remove('hide');
+    modalInfo.innerHTML = `
+    <p> Nombre:  ${copyLol[position].name}</p>`;
+  }
+});
+
+// Evento cerrar Modal
+document.getElementById('close').addEventListener('click', () => {
+  document.getElementById('myModal').classList.add('hide');
 });
